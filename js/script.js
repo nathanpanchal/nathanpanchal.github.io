70ddc0e62c9724aa2c90f0fb09113212d5987d86
Nathan Panchal <nathanpanchal@gmail.com>
2015-05-05 19:39:41 -0700

    np - add page specific sytlesheets and scripts

--- js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('.nav-menu-button').click(function() {
-        $('.page').addClass('is-navigating');
-    });
-    $('.page-content-overlay').click(function() {
-        $('.page').removeClass('is-navigating');
-    });
-});
