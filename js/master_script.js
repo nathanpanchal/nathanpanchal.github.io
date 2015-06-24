40a8f9910e1f571c599cb4e6e213e2d84ed51cea
Nathan Panchal <nathanpanchal@gmail.com>
2015-06-23 20:36:30 -0700

    np - remove interaction with page-content when navigating

--- js/master_script.js
+++ js/master_script.js
@@ -1,6 +1,8 @@
 $('.nav-menu-button').click(function() {
     $('.page').addClass('is-navigating');
+    $('html, body').addClass('is-not-scrollable');
 });
 $('.page-content-overlay').click(function() {
     $('.page').removeClass('is-navigating');
+    $('html, body').removeClass('is-not-scrollable');
 });
